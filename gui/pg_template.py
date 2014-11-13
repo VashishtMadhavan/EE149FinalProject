f1dbbe9c39fa0d13ebdd5c22736996f69a17e8aa
Stephen Martinis <moowiz2020@gmail.com>
2014-11-13 00:52:17 -0800

    Basic code that shows a hand

--- gui/pg_template.py
+++ gui/pg_template.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
- A pygame template that has the most common setup items
- Does not break into a main( ) and uses global data for simplicity
- This template is intended to be simple and easy to follow, not optimized by any means
- 
- My pygame_helpers.py is recommended as a companion, as the Sprites used
- in the drawing are of types defined in this file, 
- otherwise comment out/remove these examples
-
- Written by Alex Kuhl
- http://www.alexkuhl.org/teaching/pygame
- Licensed under a Creative Commons Attribution-Share Alike 3.0 Unported License. 
- Must keep this entire notice in both this file and derivative works
-'''
-
 import pygame, random, sys
 from pygame.locals import *
-from pygame_helpers import *
+from pygame import sprite, image
 
 # initialize pygame
 pygame.init( )
@@ -26,19 +11,28 @@ pygame.init( )
 # setup the screen
 scr_size = ( 800, 600 )  # first is width, second height
 screen = pygame.display.set_mode( scr_size )
-pygame.display.set_caption( "Game Title" )
+pygame.display.set_caption("DDR")
+
+class Hand(sprite.DirtySprite):
+  hand_list = sprite.Group()
+  def __init__(self):
+    super(Hand, self).__init__()
+
+    self.image = self.load_image()
+    self.rect = self.image.get_rect()
+    Hand.hand_list.add(self)
+
+  def load_image(self):
+    return image.load('hand.jpg').convert()
 
 # load the background
 # solid colors
-background = pygame.Surface( scr_size )
-background.fill( ( 0, 0, 0 ) ) # RGB colors
-# for an image...
-# the load_image arguments are filname, whether or not the image uses alpha, and whether uses a colorkey (we won't use colorkey)
-# background = load_image( filename, True, False )[ 0 ]
-# background = pygame.transform.scale( background, scr_size )
+background = pygame.Surface(scr_size)
+background = background.convert()
+background.fill((0, 0, 0)) # RGB colors
 
 # draw background and update display
-screen.blit( background, ( 0,0 ) )
+screen.blit(background, (0, 0))
 pygame.display.update( ) 
 
 pygame.mouse.set_visible( True )
@@ -50,6 +44,8 @@ clock = pygame.time.Clock( )
 # global data
 
 # create groups
+all_hands = []
+all_hands.append(Hand())
 
 # the game loop -- go!
 while True:
@@ -62,14 +58,12 @@ while True:
   # get ticks since last call
   time_passed = clock.tick( 60 ) # tick takes optional argument that is an int for max frames per second
 
+  for hand in all_hands:
+      hand.rect.move_ip(1, 0)
+
   # collision handling and game updates would go here
  
   # draw!
-  drawgrp.clear( screen, background )
-  rectlist = drawgrp.draw( screen )
-  pygame.display.update( rectlist )
-  #screen.blit( background, ( 0,0 ) )
-  #dot.blit( screen )
-  #pygame.display.flip( )
-
-# end while (game loop)
+  screen.blit(background, (0, 0))
+  Hand.hand_list.draw(background)
+  pygame.display.flip()
